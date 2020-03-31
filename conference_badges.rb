8221bf1b00567550f515ca380a25655df4cceabe
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-03-31 00:53:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,30 +1,4 @@
-def badge_maker(name)
-  "Hello, my name is #{name}."
-end
-
-def batch_badge_creator(names) 
-   badges=[]
-  names.each do |name|
- badges <<  "Hello, my name is #{name}." 
-end
-badges
-end
-
-def assign_rooms(speakers)
-room_assignment = []
-number = 1
-speakers.each do |name|
-  room_assignment << "Hello, #{name}! You'll be assigned to room #{number}!"
-  number +=1
-end
-room_assignment
-end
-
-def printer(attendees)
-badges.each do |name|
-  puts "#{name}"
-end
-assign_rooms(name).each do |name|
+zassign_rooms(name).each do |name|
   puts "#{attendees}"
 end
 end
